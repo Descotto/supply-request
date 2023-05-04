b4760fa021eefb8885d0c27e00cab45dd61c45be
Descotto <davidescotto@hotmail.com>
2023-05-04 15:50:53 -0700

    secondaries added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Field Technician</h1>
+        <h2>Secondary</h2>
+        <p id="black"><select>
+          <option value="3">0</option>
+          <option value="10">1</option>
+          <option value="20">2</option>
+          <option value="35">3</option>
+          <option value="50">4</option>
+          <option value="75">5</option>
+          <option value="100">6</option>
+        </select> Meters (black) <select>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select></p>
+        <br></br>
+        <p id="white"><select>
+          <option value="3">0</option>
+          <option value="10">1</option>
+          <option value="20">2</option>
+          <option value="35">3</option>
+        </select> Meters (White) <select>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select></p>
+      </div>
     </div>
   );
 }
