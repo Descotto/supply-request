f32a1da0126c6ba4c581473aef4ed834a4fca03c
calvickauer <calvickauer@gmail.com>
2023-09-01 11:13:29 -0700

    about styling and navbar functionality

--- src/About.js
+++ src/About.js
@@ -1,39 +1,31 @@
 import React from 'react';
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.css';
-
-
 
 
 function About() {
+  return (
+    <div className='about-container'>
+      <h1>About</h1>
 
+      <h3>How to use</h3>
+      <p className="howto">
+        <ul>
+          <li><strong>New Request:</strong>Enter your name and lead's name and click the "Complete" button at the bottom. <br /> Your Request ID will be saved to the clipboard, and the "Show Order" button will take you to the Order tab.</li><br />
+          <li><strong>Find by ID:</strong>Enter the order ID in the entry field and click "Search". <br /> This will take you to the Order tab.</li><br />
+          <li><strong>Order Tab:</strong>Here you can view and confirm the request made, as well as downloading an excel sheet with the information. <br /> To download the excel, click the "Download Excel". <br /> The excel sheet will save as "Supply_Request_(date it was created).xlsx". <br /> Orders will remain in the database for up to 2 weeks so that this can remain free.</li><br />
+        </ul>
+      </p>
 
-    return (
-        <div className='App'>
-            <h1>About</h1>
+      <h4>Contact</h4>
+      <div className="social-links">
+        <a href="https://www.linkedin.com/in/david-escotto/" className="linkedin"><i className="bi bi-linkedin">linkedin</i></a><br />
+        <small>Email: davidescotto05@gmail.com</small>
+      </div>
 
-            <h3>How to use</h3>
-            <p className="howto">
-                <ul>
-                    <li><strong>New Request:</strong>Enter you name and lead's name and click the "Complete" button at the bottom. <br /> Your Request ID will be saved to the clipboard and the "Show Order" button will take to the Order tab.</li><br />
-                    <li><strong>Find by ID:</strong>Enter the order ID in the entry field and click "Search". <br /> This will take you to the Order tab.</li><br />
-                    <li><strong>Order Tab:</strong>Here you can view and confirm the request made, as well as downloading an excel sheet with the information. <br /> To download the excel click the "Download Excel". <br /> The excel sheet will save as "Supply_Request_(date it was created).xlsx". <br /> Orders will remain in the database for up to 2 weeks so that this can remain free.</li><br />
-                </ul>
-            </p>
-
-            <h4>Contact</h4>
-            <div className="social-links">
-                <a href="https://www.linkedin.com/in/david-escotto/" className="linkedin"><i className="bi bi-linkedin">linkedin</i></a><br />
-                <small>Email: davidescotto05@gmail.com</small>
-            </div>
-
-            <footer>
-                <small>Created by David Escotto</small>
-            </footer>
-        </div>
-    )
+      <footer>
+        <small>Created by David Escotto</small>
+      </footer>
+    </div>
+  );
 }
-
-
 
 export default About;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const LeftSidebarNavbar = (props) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleNavbar = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleLinkClick = () => {
+    setExpanded(false); // Close the navbar when a link is clicked
+  };
+
   return (
-    <Navbar className="custom-navbar" expand="lg">
-      <Navbar.Brand href="/">Home</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar
+      className={`custom-navbar ${expanded ? 'expanded' : ''}`}
+      bg="light"
+      expand="lg"
+      expanded={expanded}
+      onSelect={() => {}} // This is required for collapseOnSelect to work
+      collapseOnSelect // Automatically close the Navbar on link selection
+    >
+      <div className="navbar-top">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleNavbar} />
+        <Navbar.Brand href="/" className={expanded ? 'expanded-home-link' : ''}>
+          Home
+        </Navbar.Brand>
+      </div>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="custom-nav">
-          <props.Link to="/request" className="custom-nav-link">New Request</props.Link>
-          <props.Link to="/lookup" className="custom-nav-link">Find by ID</props.Link>
-          <props.Link to="/about" className="custom-nav-link">About</props.Link>
+          <props.Link
+            to="/request"
+            className="custom-nav-link"
+            onClick={handleLinkClick} // Close the navbar when this link is clicked
+          >
+            New Request
+          </props.Link>
+          <props.Link
+            to="/lookup"
+            className="custom-nav-link"
+            onClick={handleLinkClick} // Close the navbar when this link is clicked
+          >
+            Find by ID
+          </props.Link>
+          <props.Link
+            to="/about"
+            className="custom-nav-link"
+            onClick={handleLinkClick} // Close the navbar when this link is clicked
+          >
+            About
+          </props.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
