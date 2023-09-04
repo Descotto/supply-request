6923c3cd7a691561553bffbd13ac8389dd155d56
calvickauer <calvickauer@gmail.com>
2023-09-04 00:19:07 -0700

    optimize accessories and zips

--- src/Zips.js
+++ src/Zips.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
 
-
-
-
-
-
 function Zips(props) {
   const { handleChange } = props;
-
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   const toggleCollapse = () => {
     setIsCollapsed(!isCollapsed);
   };
 
+  const renderDropdownOptions = (maxOptions) => {
+    const options = [];
+    for (let i = 0; i <= maxOptions; i++) {
+      options.push(<option key={i} value={i}>{i}</option>);
+    }
+    return options;
+  };
+
   return (
     <div>
-      <button class="btn btn-secondary custom-width btn-color1" onClick={toggleCollapse}>
+      <button className="btn btn-secondary custom-width btn-color1" onClick={toggleCollapse}>
         {isCollapsed ? 'Zip Ties & Flex Clips' : 'Collapse'}
       </button>
       {!isCollapsed && (
@@ -25,134 +27,34 @@ function Zips(props) {
             <div className='zips items-container'>
               <h2 className='items-title'>Zip Ties & Flex Clips</h2>
               <p onChange={handleChange}>8" Zip Ties Black 100/bag or 1,000/bag - <select id='CP-8-40-B' name='8" Zip Ties Black 100/bag or 1,000/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>8" Zip Ties Natural 100/bag or 1,000/bag - <select id='CP-8-40-N' name='8" Zip Ties Natural 100/bag or 1,000/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>8" Screwdown Zip Ties Black 100/bag - <select id='CP-7-50MH-B' name='8" Screwdown Zip Ties Black 100/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>8" Screwdown Zip Ties Natural 100/bag - <select id='CP-7-50MH-N' name='8" Screwdown Zip Ties Natural 100/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>11" Zip Tie Black 100/bag or 1,000/bag - <select id='CP-11-50-B' name='11" Zip Tie Black 100/bag or 1,000/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>14" Screwdown Zip Ties Black 100/bag - <select id='CP-14-50MH-B' name='14" Screwdown Zip Ties Black 100/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>Small Flex Clips Black 100/bag - <select id='G4BK-03' name='Small Flex Clips Black 100/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>Large Flex Clips Black 100/bag - <select id='G4BK-05' name='Large Flex Clips Black 100/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>Small Flex Clips White 100/bag - <select id='G4WH-03' name='Small Flex Clips White 100/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
               <p onChange={handleChange}>Large Flex Clips White 100/bag - <select id='G4WH-05' name='Large Flex Clips White 100/bag'>
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {renderDropdownOptions(10)}
               </select></p>
             </div>
           </div>
