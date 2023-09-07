f9944ffae9e94e1dd8d681f37cbfa2f3c061178a
Descotto <davidescotto@hotmail.com>
2023-09-06 21:33:41 -0700

    add response for no name found

--- src/ByName.js
+++ src/ByName.js
@@ -5,32 +5,39 @@ import { Navigate } from 'react-router-dom';
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function ByName(props) {
-    const [redirect, setRedirect] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+  const [showResult, setShowResult] = useState('');
 
+  function handleSubmitName(e) {
+    e.preventDefault();
+    let name = e.target.order.value.toUpperCase();
 
-    function handleSubmitName(e) {
-        e.preventDefault();
-        let name = e.target.order.value.toUpperCase();
-    
-        axios.get(`${REACT_APP_SERVER_URL}byname/${name}`)
-          .then((response) => {
-            props.setOrder(response.data);
-            setRedirect(true);
-          }).catch((error) => {
-            console.log("ERROR", error);
-          });
-      }
-      if (redirect) return <Navigate to="/results" />;
+    axios.get(`${REACT_APP_SERVER_URL}byname/${name}`)
+      .then((response) => {
+        if (response.data.order.length > 0) {
+          props.setOrder(response.data);
+          setRedirect(true);
+        }else {
+          setShowResult('No Match Found');
+        }
+          
+
+      }).catch((error) => {
+        console.log("ERROR", error);
+      });
+  }
+  if (redirect) return <Navigate to="/results" />;
 
   return (
     <div className="order-container">
-      
 
       <form onSubmit={handleSubmitName}>
         <h4>Search by Name</h4>
         <input autoComplete='off' name='order' type="text" placeholder="Enter Name" />
         <button type='button submit' className='btn btn-primary'>Search</button>
       </form>
+      <br />
+      <h5>{showResult}</h5>
 
     </div>
   );
