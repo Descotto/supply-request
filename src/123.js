bc4c9da0e1af48e47bbee6d1e296f5eef38bc001
calvickauer <calvickauer@gmail.com>
2023-09-23 20:05:47 -0700

    navbar styling

--- src/123.js
+++ src/123.js
@@ -5,6 +5,29 @@ import './Modal.css';
 const Modal = ({ isOpen, onClose, title, children }) => {
   if (!isOpen) return null;
 
+  function ByName(props) {
+    const [redirect, setRedirect] = useState(false);
+    const [showResult, setShowResult] = useState('');
+  
+    function handleSubmitName(e) {
+      e.preventDefault();
+      let targetName = e.target.order.value.toUpperCase();
+  
+      axios.get(`${REACT_APP_SERVER_URL}byname/${targetName}`)
+        .then((response) => {
+          if (response.data.order.length > 0) {
+            props.setOrder(response.data);
+            setRedirect(true);
+          }else {
+            setShowResult('No Match Found');
+          }
+            
+  
+        }).catch((error) => {
+          console.log("ERROR", error);
+        });
+    }
+
   return ReactDOM.createPortal(
     <div className="modal-overlay">
       <div className="modal">
